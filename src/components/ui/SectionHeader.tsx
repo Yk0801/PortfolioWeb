b2e2017d421bcbf2e4e2dcc2702e499a28bfcb48
Yuktha sri Bollina <yukthasri005@gmail.com>
2025-05-07 15:42:28 +0530

    Update SectionHeader.tsx

--- src/components/ui/SectionHeader.tsx
+++ src/components/ui/SectionHeader.tsx
@@ -1,11 +1,19 @@
 
+import { ReactNode } from 'react';
+
+interface SectionHeaderProps {
+  title: string;
+  subtitle?: string | ReactNode;
+  centered?: boolean;
+}
+
 const SectionHeader = ({ 
   title, 
   subtitle, 
   centered = false 
 }: SectionHeaderProps) => {
   return (
-    <div className={`pt-24 mb-12 ${centered ? 'text-center' : ''}`}>
+    <div className={`mb-12 ${centered ? 'text-center' : ''}`}>
       <h2 className={`section-title ${centered ? 'mx-auto after:left-0 after:right-0 after:mx-auto' : ''}`}>
         {title}
       </h2>
@@ -18,3 +26,4 @@ const SectionHeader = ({
   );
 };
 
+export default SectionHeader;
